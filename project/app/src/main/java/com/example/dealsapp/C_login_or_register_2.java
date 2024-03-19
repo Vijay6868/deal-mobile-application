f80f62adeb9d1a936a050bbd129c7b196e8a8028
Vijay Kumar <saroyav3@gmail.com>
2024-03-19 20:11:23 +1300

    activityc_login_or_register2 added

---
+++ project/app/src/main/java/com/example/dealsapp/C_login_or_register_2.java
@@ -0,0 +1,14 @@
+package com.example.dealsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class C_login_or_register_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clogin_or_register2);
+    }
+}
