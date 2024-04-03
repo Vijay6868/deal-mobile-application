51863c41eb90a3b4933a9982668adbfabfd7f398
Vijay Kumar <saroyav3@gmail.com>
2024-04-04 10:21:13 +1300

    Homescreen updated

--- project/app/src/main/java/com/example/dealsapp/C_homeScreen.java
+++ project/app/src/main/java/com/example/dealsapp/C_homeScreen.java
@@ -1,30 +1,72 @@
 package com.example.dealsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class C_homeScreen extends AppCompatActivity {
     RecyclerView recyclerView;
+    TabLayout tabLayout;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chome_screen);
-        ArrayList<Deal> deals = new ArrayList<>(); // to store Deal class object
+//        ArrayList<Deal> deals = new ArrayList<>(); // to store Deal class object
+//
+//        ArrayList<Integer> list1 = new ArrayList<Integer>(); // to store image res id for s listing
+//        list1.add(R.drawable.img1);
+//        list1.add(R.drawable.img2);
+//        list1.add(R.drawable.img3);
+//
+//        deals.add(new Deal("Pure Milford Sound Cruise",99,list1,5));
+//        recyclerView = findViewById(R.id.homeScreen);
+//        RVAdapter adapter = new RVAdapter(deals);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setAdapter(adapter);
 
-        ArrayList<Integer> list1 = new ArrayList<Integer>(); // to store image res id for s listing
-        list1.add(R.drawable.img1);
-        list1.add(R.drawable.img2);
-        list1.add(R.drawable.img3);
+        tabLayout = findViewById(R.id.home_tab);
+        frameLayout = findViewById(R.id.frame_layout);
 
-        deals.add(new Deal("Pure Milford Sound Cruise",99,list1,5));
-        recyclerView = findViewById(R.id.homeScreen);
-        RVAdapter adapter = new RVAdapter(deals);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new F_Featured_Deals())
+                .addToBackStack(null)
+                .commit();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment = null;
+                switch (tab.getPosition()){
+                    case 0:
+                        fragment = new F_Featured_Deals();
+
+                    case 1:
+                        fragment = new Category_frag();
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
---
+++ project/app/src/main/java/com/example/dealsapp/F_Featured_Deals.java
@@ -0,0 +1,38 @@
+package com.example.dealsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class F_Featured_Deals extends Fragment {
+
+    RecyclerView recyclerView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_f__featured__deals, container, false);
+
+        ArrayList<Deal> deals = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<Integer>(); // to store image res id for s listing
+        list1.add(R.drawable.img1);
+        list1.add(R.drawable.img2);
+        list1.add(R.drawable.img3);
+
+        deals.add(new Deal("Pure Milford Sound Cruise",99,list1,5));
+        recyclerView = view.findViewById(R.id.f_rv_item);
+        RVAdapter adapter = new RVAdapter(deals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+}
