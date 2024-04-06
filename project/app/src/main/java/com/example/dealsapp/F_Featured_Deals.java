5963c1d236d8eec28ede827f37ac558a3299a559
Vijay Kumar <saroyav3@gmail.com>
2024-04-06 20:58:50 +1300

    Deal description activity added

--- project/app/src/main/java/com/example/dealsapp/F_Featured_Deals.java
+++ project/app/src/main/java/com/example/dealsapp/F_Featured_Deals.java
@@ -23,7 +23,7 @@ public class F_Featured_Deals extends Fragment {
         View view = inflater.inflate(R.layout.fragment_f__featured__deals, container, false);
 
         ArrayList<Deal> deals = new ArrayList<>();
-        ArrayList<Integer> list1 = new ArrayList<Integer>(); // to store image res id for s listing
+        ArrayList<Integer> list1 = new ArrayList<>(); // to store image res id for s listing
         list1.add(R.drawable.img1);
         list1.add(R.drawable.img2);
         list1.add(R.drawable.img3);
