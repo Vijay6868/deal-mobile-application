b13be83b298c0a37a8525b44c1ac4a51736f19ad
Vijay Kumar <saroyav3@gmail.com>
2024-04-18 16:29:40 +1200

    finished with available deals fragement

--- project/app/src/main/java/com/example/dealsapp/F_Availble_Deals.java
+++ project/app/src/main/java/com/example/dealsapp/F_Availble_Deals.java
@@ -11,12 +11,17 @@ import android.view.ViewGroup;
 
 public class F_Availble_Deals extends Fragment {
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_f__availble__deals, container, false);
+
+        // Use getChildFragmentManager() instead of getSupportFragmentManager()
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.main_frame_av_de, new F_Featured_Deals())
+                .commit();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f__availble__deals, container, false);
+        return view;
     }
 }
